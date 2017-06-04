31aaa3d21118a969c56f5fffd43f88b379704f3e
wiseking1984 <wiseking1984@163.com>
2017-06-04 11:16:22 +0800

    debug 2

--- ui/main.py
+++ ui/main.py
@@ -14,15 +14,18 @@ urls=(
       '/','index'
 )
 msghandler.basic = accessToken.Basic()
-accessTask = threading.Thread(target=msghandler.basic.run(),args=())
+accessTask = threading.Thread(target=msghandler.basic,args=())
 
-app = web.application(urls,globals())
+
 if __name__ == "__main__": 
-    accessTask.setDaemon(True)
-    accessTask.run()
+    print "start main"    
+    app = web.application(urls,globals())
     app.run()
+    accessTask.setDaemon(True)
+    accessTask.start()
+    print "app run"
 
 #for test only
 class index:
-    def GET(self):
-        return "Hello world!" 
+    def GET(self):        
+        return "Hello world!" +msghandler.basic.get_access_token()
